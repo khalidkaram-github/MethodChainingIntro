556ec1ae94cdf27d4c3252263dd241bc8732d645
khalid karam <khalid.tasks@gmail.com>
2024-09-02 14:52:32 +0300

    .

--- Program.cs
+++ Program.cs
@@ -5,15 +5,20 @@
 
         static void Main()
         {
+            var original = "Original";
 
+            var result = original.Append(" new string").Append(" dfghjj").Append(" dadadasd");
 
+            Console.WriteLine(result);
 
         }
     }
-    static class MathOperations
+    public static class StringExtensions
     {
-        public static int Double(int x) { return x * 2; }
-        public static int Square(int x) { return x * x; }
-        public static int Increment(int x) { return x + 1; }
+        public static string Append(this string source, string str)
+        {
+            return source + str;
+        }
+
     }
 }
