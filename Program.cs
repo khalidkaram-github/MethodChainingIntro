1daf7e4d82801cf317fd4fbd691f53eb64891520
khalid karam <khalid.tasks@gmail.com>
2024-09-02 14:01:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MethodChainingIntro
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+
+
+
+        }
+    }
+    static class MathOperations
+    {
+        public static int Double(int x) { return x * 2; }
+        public static int Square(int x) { return x * x; }
+        public static int Increment(int x) { return x + 1; }
+    }
+}
